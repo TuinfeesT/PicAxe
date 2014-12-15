0111c6ddf446624b5480881c4bdaf17e9f637f3c
Peter Wagenaar <sakartu@gmail.com>
2014-12-15 10:44:46 +0100

    Add SITE_ID for django.contrib.sites

--- picaxe/settings.py
+++ picaxe/settings.py
@@ -79,11 +79,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Site stuff
+SITE_ID = 1
 
 try:
     from local_settings import *
