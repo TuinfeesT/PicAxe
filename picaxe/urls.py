41deccb4cde9d553db021f1da90759b4b1b14665
Peter Wagenaar <sakartu@gmail.com>
2014-12-15 10:57:33 +0100

    Remove django.contrib.sites from admin interface

--- picaxe/urls.py
+++ picaxe/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.sites.models import Site
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,3 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'photologue/', include('photologue.urls', namespace='photologue')),
 )
+
+admin.site.unregister(Site)
