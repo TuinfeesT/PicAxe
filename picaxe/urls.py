1098d8278864ec850e56c89812875a196dcdb315
Peter Wagenaar <sakartu@gmail.com>
2014-12-15 10:43:03 +0100

    Add photologue to urls.py

--- picaxe/urls.py
+++ picaxe/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'photologue/', include('photologue.urls', namespace='photologue')),
 )
