d57c73b263a401bc88f5547f52f1e02a548ba3e1
Peter Wagenaar <sakartu@gmail.com>
2014-12-18 15:11:23 +0100

    Add MEDIA to urlpatterns for debugging purposes

--- picaxe/urls.py
+++ picaxe/urls.py
@@ -1,8 +1,13 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('photologue.urls', namespace='photologue')),
 )
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
